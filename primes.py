72abcaadefe32f3538e8aa08f8363c29914ad962
4v0n <navodhk@hotmail.com>
2022-10-12 20:29:37 +0100

    Throw error when input at or below 0

--- primes.py
+++ primes.py
@@ -2,5 +2,26 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+
+    if (number_of_primes <= 0) :
+        raise ValueError("number_of_primes must be above 0")
+
+
     list = []
+    num = 1
+
+    while (len(list) < number_of_primes):
+
+        num+=1
+        prime = True
+
+        for n in range(num):
+
+            if ((n > 1) and ((num % n) == 0)):
+                prime = False
+                break
+        
+        if prime:
+            list.append(num)
+
     return list
